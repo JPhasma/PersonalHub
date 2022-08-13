91682918f2eb44f9e261dc79f0724308d83f41f3
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-08-13 17:35:39 +0100

    added excercises page

---
+++ pages/excercises/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+// import Head from 'next/head';
+// import Image from 'next/image';
+// import styles from '../styles/Home.module.css';
+
+const Excercises: NextPage = () => {
+  return (
+    <div>
+      <h1>Excercises</h1>
+      <p>Below are some coding challenges and excercies I've been working on</p>
+    </div>
+  );
+};
+
+export default Excercises;
