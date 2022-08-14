cc5319b8e74bf89ff89d15c44c5de32d87501bb9
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-08-14 14:36:31 +0100

    next project

--- pages/coding/lister/lister.test.jsx
+++ pages/coding/lister/lister.test.jsx
@@ -1,7 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 
 import Lister from '../lister/index';
-const checker = 'check';
 
 test('check Lister renders', () => {
   render(<Lister />);
